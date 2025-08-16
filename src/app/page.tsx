269b5fa934f6108cfc761885c86823a500fc12ac
Žan Mlakar <148876659+Bigfatonion@users.noreply.github.com>
2025-08-16 21:42:49 +0200

    Feat: Working on hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,19 @@
 import React from 'react'
 import gsap from 'gsap'
 import { ScrollTrigger,SplitText } from 'gsap/all'
+import Navbar from '@/components/Navbar'
+import Hero from '@/components/Hero'
 
 gsap.registerPlugin(ScrollTrigger, SplitText)
 
 export default function page() {
   return (
-    <div className='flex-center h-[100vh]'>
-      <h1 className='text-3xl text-indigo-300'>Hello, GSAP!</h1>
-    </div>
+    <main>
+      <Navbar/>
+      <Hero/>
+      <div className='h-dvh bg-black'>
+        
+      </div>
+    </main>
   )
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,54 @@
+import { navLinks } from "@/constants";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import React from "react";
+
+const Navbar = () => {
+
+    useGSAP(() => {
+        const navTween = gsap.timeline({
+            scrollTrigger: {
+                trigger: "nav",
+                start: "bottom top",
+            },
+        });
+
+        navTween.fromTo(
+            "nav",
+            {
+                backgroundColor: "transparent",
+            },
+            {
+                backgroundColor: "#00000050",
+                backgroundFilter: "blur(10px)",
+                duration: 1,
+                ease: "power1.inOut",
+            }
+        );
+    });
+
+    return (
+        <nav>
+            <div>
+                <a href="#home" className="flex items-center gap-2">
+                    <img
+                        src="/images/logo.png"
+                        alt="logo"
+                        width={32}
+                        height={32}
+                    />
+                    <p>Velvet Pour</p>
+                </a>
+                <ul>
+                    {navLinks.map((link) => (
+                        <li key={link.id}>
+                            <a href={`#${link.id}`}>{link.title}</a>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
