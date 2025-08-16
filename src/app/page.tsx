fd865dbd7cfe919fb8ab6712e8909ee8c0613924
Žan Mlakar <148876659+Bigfatonion@users.noreply.github.com>
2025-08-17 00:00:47 +0200

    implemented about section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import { CSSRulePlugin, ScrollTrigger, SplitText } from "gsap/all";
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import Cocktails from "@/components/Cocktails";
+import About from "@/components/About";
 
 gsap.registerPlugin(ScrollTrigger, SplitText,CSSRulePlugin);
 
@@ -14,6 +15,7 @@ export default function page() {
             <Navbar />
             <Hero />
             <Cocktails/>
+            <About/>
         </main>
     );
 }
