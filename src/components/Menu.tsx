5311cecbf57e9cc3a9d11e5eba5aa89d6135e4f9
Žan Mlakar <148876659+Bigfatonion@users.noreply.github.com>
2025-08-17 02:15:52 +0200

    animation bug fix

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -58,16 +58,6 @@ export default function Menu() {
                 ease: "power1.inOut",
             }
         );
-        gsap.timeline({
-            scrollTrigger: {
-                trigger: "#menu",
-                start: "top top",
-                end: "bottom top",
-                scrub: true,
-            },
-        })
-            .to("#m-right-leaf", { y: 200 }, 0)
-            .to("#m-left-leaf", { y: 200 }, 0);
     }, [currentIndex]);
 
     function goToSlide(index: number) {
